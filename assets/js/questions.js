2a4efdd00279c2dc1d7b5697ffa1347eb529e0ce
cornishcoder1 <leahfisher07@hotmail.co.uk>
2021-12-16 14:59:32 +0000

    Add questions array to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,91 @@
+const questionArray = [
+    {
+        question: "This dish is a speciality in the region of Gunma, typically consisting of pork tenderloin which is breaded and deep fried.",
+        correctAnswer: "Tonkatsu",
+        incorrectAnswers : [
+                            "Menchikatsu",
+                             "Katsudon",
+                            ]
+    },
+
+    {
+        question: "A type of sushi consisting of pressed rice, usually topped with fish.",
+        correctAnswer: "Nigiri",
+        incorrectAnswers : [
+                            "Sashimi",
+                             "Maki",
+                            ]
+    },
+
+    {
+        question: "These boiled dumplings are usually filled with ground meat and vegetables, served in a very light broth.",
+        correctAnswer: "Sui gyoza",
+        incorrectAnswers : [
+                            "Yaki gyoza",
+                             "Age gyoza",
+                            ]
+    },
+
+    {
+        question: "Katsuobushi shavings (or bonito flakes) are derived from which fish?",
+        correctAnswer: "Tuna",
+        incorrectAnswers : [
+                            "Salmon",
+                             "Mackerel",
+                            ]
+    },
+
+    {
+        question: "What is Japanese horseradish otherwise known as? ",
+        correctAnswer: "Wasabi",
+        incorrectAnswers : [
+                            "Onigiri",
+                            "Karaage",
+                            ]
+    },
+
+    {
+        question: "Found all over Japan in street food markets and sushi restaurants, this fried rolled omelette is typically seasoned with salt and dashi.",
+        correctAnswer: "Tamagoyaki",
+        incorrectAnswers : [
+                            "Datemaki",
+                            "Okonomiyaki",
+                            ]
+    },
+
+    {
+        question: "Which of these Japanese islands is famous for it’s Tarabagani (King Crab)?",
+        correctAnswer: "Hokkaido",
+        incorrectAnswers : [
+                            "Tashiro",
+                            "Sado",
+                            ]
+    },
+
+    {
+        question: "Which Japanese fruit is said to be one of the most expensive in the world?",
+        correctAnswer: "Yubari melon",
+        incorrectAnswers : [
+                            "Momo peach",
+                            "Akebi",
+                            ]
+    },
+
+    {
+        question: "Which variety of beef is identified by it’s fatty, well-marbled texture? ",
+        correctAnswer: "Kobe",
+        incorrectAnswers : [
+                            "Yonezawa",
+                            "Mishima",
+                            ]
+    },
+
+    {
+        question: "What fish-shaped pancake is traditionally filled with sweetened adzuki beans?",
+        correctAnswer: "Taiyaki",
+        incorrectAnswers : [
+                            "Zabuton Dora",
+                            "Mitarashi Dango",
+                            ]
+    }
+]    
