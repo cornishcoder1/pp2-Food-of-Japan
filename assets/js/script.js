254fd610bf0cf875dd656627ce3015bfa9de5d40
cornishcoder1 <leahfisher07@hotmail.co.uk>
2021-12-18 13:46:31 +0000

    move all js into one file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,88 @@
-console.log('hello!');
+console.log ('hello')
 
-let questionCounter = 0;
-let score = 0;
-const maxQuestions = 10;
+//Quiz questions, inspired by the YouTube channels 'Abroad in Japan' and 'I will always travel for food'
 
-const progressText = document.getElementById('progressText');
-const progressBarFull = document.getElementById('progressBarFull')
+let questionArray = [
 
-let availableQuestions = questionArray;
-let currentQuestion = {};
+    {
+        question:"This dish is a speciality in the region of Gunma, typically consisting of pork tenderloin which is breaded and deep fried.",
+        1:"Menchikatsu",
+        2:"Katsudon",
+        3:"Tonkatsu",
+        correctAnswer:"3",
+    },
 
+    {
+        question:"A type of sushi, consisting of pressed rice usually topped with fish.",
+        1:"Sashimi",
+        2:"Nigiri",
+        3:"Maki",
+        correctAnswer:"2",
+    },
 
-//Generate a new random question from the questionArray. Update progress text and bar and add in question to HTML. Once complete save score and load game over page. This function is with help from Brian Design and has been edited to suit this application.
+    {
+        question:"These boiled dumplings are usually served in a very light broth, filled with ground meat and vegetables.",
+        1:"Yaki gyoza",
+        2:"Sui gyoza",
+        3:"Age gyoza",
+        correctAnswer:"2",
+    },
 
-function getNewQuestion () {
-    if(questionCounter >= maxQuestions) {
-        localStorage.setItem('currentRoundScore', score);
-        return window.location.assign("./game-over.html");
-    }
+    {
+        question:"Katsuobushi shavings (or bonito flakes) are derived from which fish?",
+        1:"Tuna",
+        2:"Salmon",
+        3:"Mackerel",
+        correctAnswer:"1",
+    },
 
-    //Increment through questions
-    questionCounter ++;
+    {
+        question:"What is Japanese Horseradish also known as? ",
+        1:"Onigiri",
+        2:"Karaage",
+        3:"Wasabi",
+        correctAnswer:"3",
+    },
 
-    //Update progress counter and width element of progress bar.
-    progressText.innerText = `Question ${questionCounter} / ${maxQuestions}`;
-    progressBarFull.style.width = `${(questionCounter/maxQuestions)* 100}%`;
+    {
+        question:"Found all over Japan in street food markets and sushi restaurants, this fried rolled omelette is typically seasoned with salt and dashi.",
+        1:"Tamagoyaki",
+        2:"Datemaki",
+        3:"Okonomiyaki",
+        correctAnswer:"1",
+    },
 
-    //select random question
-    const questionSelector = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionSelector];
-    questionText.innerText = currentQuestion.question;
-  
-    //sort answers by the dataset within html
-    answers.forEach(answer => {
-    const number = answer.dataset.number;
-        answer.innerText = currentQuestion[number];
-    });
-  
-    availableQuestions.splice(questionSelector, 1);
-  
-    acceptingAnswers = true;
+    {
+        question:"Which of these Japanese islands is famous for it’s Tarabagani (King Crab)?",
+        1:"Tashiro",
+        2:"Hokkaido",
+        3:"Sado",
+        correctAnswer:"2",
+    },
 
-}
-   
+    {
+        question:"Which Japanese fruit is said to be one of the most expensive in the world?",
+        1:"Akebi",
+        2:"Momo peach",
+        3:"Yubari melon",
+        correctAnswer:"3",
+    },
+
+    {
+        question:"Which variety of beef is identified by it’s fatty, well-marbled texture?",
+        1:"Yonezawa",
+        2:"Mishima",
+        3:"Kobe",
+        correctAnswer:"3",
+    },
+
+    {
+        question:"What fish-shaped pancake is traditionally filled with sweetened adzuki beans? ",
+        1:"Zabuton Dora",
+        2:"Taiyaki",
+        3:"Mitarashi Dango",
+        correctAnswer:"2",
+    },
+];
   
   
